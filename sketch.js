c38c330f4cd25ddac42373291c45ddddf01a17b0
zep1994 <snaxbags@protonmail.com>
2018-12-01 00:09:11 -0600

    Add initial angle and r conversion to x and y

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+var n = 0;
+var c = 2;
+
 function setup() {
+  createCanvas(400, 400);
+  background(0);
 
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+
+  var a = n * 137.5;
+  var r = c * sqrt(n);
+  var x = r * cos(a) + width / 2;
+  var y = r * sin(a) + height / 2;
+  fill(255);
+  ellipse(x, y, 8, 8);
+  n++;
 }
